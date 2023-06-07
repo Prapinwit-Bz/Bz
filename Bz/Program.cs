4beecaa6521d2d10564e1449ebdae8c65601f446
Prapinwit-Bz <prapinwit.rot@sbac.ac.th>
2023-06-07 14:52:52 +0700

    Add project files.

---
+++ Bz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Prapinwit
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        { 
+            for (int iterator = 0; iterator < 3; iterator++)
+            {
+                Console.WriteLine("Outer Loop:" + iterator);
+                for (int _iterator = 0; _iterator < 2; _iterator++) 
+                {
+                    Console.WriteLine("inner Loop:" + iterator);
+                }
+            }
+            string[] brandTV = { "LG", "SONY", "SAMSUNG", "TCL", "JVC" };
+
+            foreach (string television in brandTV)
+            {
+                    Console.WriteLine("Brand" + television);
+            }
+        }
+
+    }
+}
